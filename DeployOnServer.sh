bc6da82164c85e760bf0516ebd296f54cadac839
luxkun <luciano.ferraro@gmail.com>
2017-03-22 11:20:31 +0100

    DeployOnServer header

--- DeployOnServer.sh
+++ DeployOnServer.sh
@@ -1,1 +1,2 @@
-﻿ssh 30236@unbit0016.uwsgi.it "cd ~/project_2A_2017/brawler-server && git pull && make"
+﻿#!/bin/bash
+ssh 30236@unbit0016.uwsgi.it "cd ~/project_2A_2017/brawler-server && git pull && make"
