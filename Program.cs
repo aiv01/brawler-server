b2c82bf6d79697bdedcd494f8fa1ae9f11dd507e
luxkun <luciano.ferraro@gmail.com>
2017-03-22 11:13:56 +0100

    test udp server

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
 using System.Threading;
 
 namespace Brawler_server
@@ -7,10 +9,17 @@ namespace Brawler_server
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Test Server Running!");
 
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            socket.Bind(new IPEndPoint(IPAddress.Parse(args[0]), Convert.ToInt32(args[1])));
+
+            var buffer = new byte[1024];
+            EndPoint remoteEp = new IPEndPoint(0, 0);
             while (true)
             {
+                socket.ReceiveFrom(buffer, ref remoteEp);
+                socket.SendTo(buffer, remoteEp);
                 Thread.Sleep(100);
             }
         }
