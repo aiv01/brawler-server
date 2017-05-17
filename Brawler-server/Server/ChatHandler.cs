27b5b05b5966ab7d3f93b3e04def3806a547978c
Alberto Del Villano <insane96mcp@gmail.com>
2017-05-17 09:59:49 +0200

    Debug Chat

--- Brawler-server/Server/ChatHandler.cs
+++ Brawler-server/Server/ChatHandler.cs
@@ -14,14 +14,14 @@ namespace BrawlerServer.Server
             Packet = packet;
 
             JsonData = Utilities.Utilities.ParsePacketJson(packet, typeof(Json.ChatHandler));
-            
+
             // first check if user has joined
-            if (!packet.Server.HasClient(packet.RemoteEp))
-            {
-                throw new Exception($"Client with remoteEp '{packet.RemoteEp}' tried to chat but has never joined.");
-            }
+            //if (!packet.Server.HasClient(packet.RemoteEp))
+            //{
+            //    throw new Exception($"Client with remoteEp '{packet.RemoteEp}' tried to chat but has never joined.");
+            //}
             Logs.Log($"[{packet.Server.Time}] Received chat message from '{packet.RemoteEp}' with text '{JsonData.Text}'.");
-            
+
             byte[] data = new byte[512];
             Json.ClientChatted JsonChatData = new Json.ClientChatted() { Text = JsonData.Text, Name = packet.Server.GetClientFromEndPoint(packet.RemoteEp).Name };
             Packet ClientChattedPacket = new Packet(packet.Server, data.Length, data, null);
