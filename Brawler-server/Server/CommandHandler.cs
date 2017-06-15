c64d9a95900a1ad8ae4b15c35f1e5b4daa0de0b1
Alberto Del Villano <insane96mcp@gmail.com>
2017-06-15 11:43:52 +0200

    Added CommandHandler Tests

--- Brawler-server/Server/CommandHandler.cs
+++ Brawler-server/Server/CommandHandler.cs
@@ -9,15 +9,13 @@ namespace BrawlerServer.Server
         public Packet Packet { get; private set; }
         public Json.CommandHandler JsonData { get; private set; }
         public string JsonSerialized { get; private set; }
-        public Client Client { get; private set; }
 
         public void Init(Packet packet)
         {
             Packet = packet;
 
             JsonData = Utilities.Utilities.ParsePacketJson(packet, typeof(Json.CommandHandler));
-
-            // create client and add it to the server's clients
+            
             Logs.Log($"[{packet.Server.Time}] Received Command {JsonData.command} from {packet.RemoteEp}.");
         }
     }
