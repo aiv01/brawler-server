4560446bc7b1f0feda0b91db1c5e2da369420fa7
Alberto Del Villano <insane96mcp@gmail.com>
2017-06-15 11:41:02 +0200

    Fixed crash

--- Brawler-server/Server/CommandHandler.cs
+++ Brawler-server/Server/CommandHandler.cs
@@ -18,7 +18,6 @@ namespace BrawlerServer.Server
             JsonData = Utilities.Utilities.ParsePacketJson(packet, typeof(Json.CommandHandler));
 
             // create client and add it to the server's clients
-            Client = packet.Server.GetClientFromAuthedEndPoint(packet.RemoteEp);
             Logs.Log($"[{packet.Server.Time}] Received Command {JsonData.command} from {packet.RemoteEp}.");
         }
     }
