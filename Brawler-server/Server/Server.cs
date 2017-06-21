73012246c7d6fce42560d467ec19f631f867a8d1
Alberto Del Villano <insane96mcp@gmail.com>
2017-06-21 11:55:55 +0200

    Fixed DeltaTime Begin not float

--- Brawler-server/Server/Server.cs
+++ Brawler-server/Server/Server.cs
@@ -130,7 +130,7 @@ namespace BrawlerServer.Server
         public bool IsRunning { get; set; }
         public uint Time { get; private set; }
         // does NOT count looptime
-        public uint DeltaTime { get; private set; }
+        public float DeltaTime { get; private set; }
 
         public uint MaxIdleTimeout { get; private set; }
 
